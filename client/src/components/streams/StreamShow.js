7f4064ea12ae148d5b3f34e098c5f08b6289f464
Jorge Quinonez <jorge@Jorges-MacBook-Pro.local>
2020-08-17 17:57:07 -0700

    Removes cached

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,13 +1,11 @@
 import React from "react";
 import flv from "flv.js";
 import { connect } from "react-redux";
-
 import { fetchStream } from "../../actions";
 
 class StreamShow extends React.Component {
   constructor(props) {
     super(props);
-
     this.videoRef = React.createRef();
   }
 
